3bd1d3fedf8ee7a05606b440e9fc6d6180e16018
yukey <yuqi.wu1@my.jcu.au>
2025-03-25 23:12:44 +0800

    add TravelerApplication file

---
+++ app/src/main/java/com/example/ass2_travaler/TravalerApplication.kt
@@ -0,0 +1,19 @@
+package com.example.ass2_travaler
+
+import android.app.Application
+
+
+import com.example.ass2_travaler.data.AppContainer
+import com.example.ass2_travaler.data.DefaultAppContainer
+
+class TravalerApplication : Application() {
+    lateinit var container: AppContainer
+
+    override fun onCreate() {
+        super.onCreate()
+        container = DefaultAppContainer()
+
+
+    }
+
+}
