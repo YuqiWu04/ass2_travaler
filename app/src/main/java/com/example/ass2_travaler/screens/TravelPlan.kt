e55ec2161350a21dc55e083b9d9a9d9f97e5c6b6
yukey <yuqi.wu1@my.jcu.au>
2025-03-27 00:10:31 +0800

    add some other two page which only has some test data

---
+++ app/src/main/java/com/example/ass2_travaler/screens/TravelPlan.kt
@@ -0,0 +1,30 @@
+package com.example.ass2_travaler.screens
+
+
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.unit.dp
+import androidx.navigation.NavController
+
+@Composable
+fun TravelPlan(
+    navController: NavController, viewModel: HomeCityViewModel
+) {
+    Column(
+        modifier = Modifier
+            .fillMaxSize()
+            .padding(16.dp),
+        verticalArrangement = Arrangement.Center,
+        horizontalAlignment = Alignment.CenterHorizontally
+    ) {
+        Text("Travel Plan Screen", style = MaterialTheme.typography.headlineMedium)
+
+    }
+}
