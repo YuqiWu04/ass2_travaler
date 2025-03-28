506f651d9a8a9486f4b0d24dbd4e7ff65474e749
yukey <yuqi.wu1@my.jcu.au>
2025-03-28 23:31:52 +0800

    update some build grade plugins and setting

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -1,8 +1,12 @@
+
+import com.android.build.api.dsl.AndroidResources
+
 plugins {
     alias(libs.plugins.android.application)
     alias(libs.plugins.kotlin.android)
     alias(libs.plugins.kotlin.compose)
     id("kotlinx-serialization")
+    id("com.google.devtools.ksp")
 }
 
 android {
@@ -12,12 +16,14 @@ android {
     defaultConfig {
         applicationId = "com.example.ass2_travaler"
         minSdk = 35
-        targetSdk = 34
+        targetSdk = 35
         versionCode = 1
         versionName = "1.0"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
     }
+
+
 
     buildTypes {
         release {
@@ -28,6 +34,8 @@ android {
             )
         }
     }
+
+
     compileOptions {
         sourceCompatibility = JavaVersion.VERSION_11
         targetCompatibility = JavaVersion.VERSION_11
@@ -38,20 +46,36 @@ android {
     buildFeatures {
         compose = true
     }
+
+    aaptOptions {
+        ignoreAssetsPattern = "!*.webp:!*.png:!*.jpg"
+        ignoreAssetsPattern  = "!.DS_Store:!.localized"
+        ignoreAssetsPattern  = "!.DS_Store:!Thumbs.db:!desktop.ini"
+        additionalParameters .add("--no-version-vectors")
+    }
+
 }
 
 dependencies {
+    // ===== AndroidX base =====
 
     implementation(libs.androidx.core.ktx)
     implementation(libs.androidx.lifecycle.runtime.ktx)
-    implementation(libs.androidx.activity.compose)
+
+    implementation("androidx.lifecycle:lifecycle-viewmodel-compose:2.6.2")
+
+    // ===== Compose =====
+
     implementation(platform(libs.androidx.compose.bom))
     implementation(libs.androidx.ui)
     implementation(libs.androidx.ui.graphics)
     implementation(libs.androidx.ui.tooling.preview)
     implementation(libs.androidx.material3)
+    implementation(libs.androidx.activity.compose)
+    //  LiveData  Compose
     implementation(libs.androidx.runtime.livedata)
 
+    // ===== test =====
     testImplementation(libs.junit)
     androidTestImplementation(libs.androidx.junit)
     androidTestImplementation(libs.androidx.espresso.core)
@@ -59,32 +83,37 @@ dependencies {
     androidTestImplementation(libs.androidx.ui.test.junit4)
     debugImplementation(libs.androidx.ui.tooling)
     debugImplementation(libs.androidx.ui.test.manifest)
-    implementation ("org.jetbrains.kotlinx:kotlinx-serialization-core:1.6.0")
-    // 如果解析 JSON，添加以下依赖：
-    implementation ("org.jetbrains.kotlinx:kotlinx-serialization-json:1.6.0")
-    implementation("androidx.core:core-ktx:1.12.0")
-    implementation("androidx.lifecycle:lifecycle-runtime-ktx:2.6.2")
-    implementation("androidx.lifecycle:lifecycle-viewmodel-compose:2.6.2")
+
+    // ===== Kotlinx Serialization =====
+    implementation("org.jetbrains.kotlinx:kotlinx-serialization-core:1.6.0")
+    implementation("org.jetbrains.kotlinx:kotlinx-serialization-json:1.6.0")
+
+    // ===== Retrofit  =====
+    implementation("com.squareup.retrofit2:retrofit:2.9.0")
+    // Gson transfer
+    implementation("com.squareup.retrofit2:converter-gson:2.9.0")
+    // Kotlinx Serialization 转换器（配合 Kotlinx Serialization 使用）
+    implementation("com.jakewharton.retrofit:retrofit2-kotlinx-serialization-converter:1.0.0")
+
+    // ===== Coil  =====
+
     implementation("io.coil-kt.coil3:coil-compose:3.1.0")
     implementation("io.coil-kt.coil3:coil-network-okhttp:3.1.0")
+
+    // ===== Navigation Compose =====
     implementation("androidx.navigation:navigation-compose:2.7.4")
 
-    // Retrofit 核心库
-    implementation ("com.squareup.retrofit2:retrofit:2.9.0")
+    // ===== Kotlin  =====
+    implementation("org.jetbrains.kotlinx:kotlinx-coroutines-core:1.7.3")
+    implementation("org.jetbrains.kotlinx:kotlinx-coroutines-android:1.7.3")
 
-    // Retrofit 的 Kotlinx Serialization 转换器（需配合 Kotlinx Serialization）
-    implementation ("com.jakewharton.retrofit:retrofit2-kotlinx-serialization-converter:1.0.0")
-    implementation ("com.squareup.retrofit2:converter-gson:2.9.0")
+    implementation ("androidx.room:room-runtime:2.6.1")
 
-    implementation ("androidx.lifecycle:lifecycle-viewmodel-compose:2.6.2")
+    // Kotlin
+    implementation ("androidx.room:room-ktx:2.6.1")
 
-    // Coil 图片加载库
-    implementation ("io.coil-kt:coil-compose:2.4.0")
-    implementation ("org.jetbrains.kotlinx:kotlinx-coroutines-core:1.7.3")
-    implementation ("org.jetbrains.kotlinx:kotlinx-coroutines-android:1.7.3")
-    // Material3 设计系统
-    implementation ("androidx.compose.material3:material3:1.1.2")
+    // kotlin-kapt
+    ksp("androidx.room:room-compiler:2.6.1")
+    implementation("org.jetbrains.kotlinx:kotlinx-metadata-jvm:0.6.0")
 
-    implementation ("com.jakewharton.retrofit:retrofit2-kotlinx-serialization-converter:1.0.0")
-    implementation ("org.jetbrains.kotlinx:kotlinx-serialization-json:1.6.0")
 }
--- build.gradle.kts
+++ build.gradle.kts
@@ -4,4 +4,12 @@ plugins {
     alias(libs.plugins.kotlin.android) apply false
     alias(libs.plugins.kotlin.compose) apply false
     id("org.jetbrains.kotlin.plugin.serialization") version "1.8.20" apply false
+    id("com.google.devtools.ksp") version "2.1.0-1.0.29" apply true
+}
+configurations.all {
+    resolutionStrategy.eachDependency {
+        if (requested.group == "org.jetbrains.kotlinx" && requested.name == "kotlinx-metadata-jvm") {
+            useVersion("0.6.0")
+        }
+    }
 }
